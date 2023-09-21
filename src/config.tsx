a913326bc18f9d9fe0471c675555a7dabefdcafa
maroneed <gabriell.appella@gmail.com>
2023-09-21 19:48:14 -0300

    Modifications in config.xml

--- src/config.tsx
+++ src/config.tsx
@@ -1,5 +1,5 @@
-// export const API_URL = "http://192.168.1.1:5000/api/";
-export const API_URL = "https://4425e53c5ed290.lhr.life/api/";
+// export const API_URL = "http://localhost:8080/api/";
+export const API_URL = "https://7ac43be9cd54b4.lhr.life/api/";
 // export const API_URL = "https://union-platform-server-api.vercel.app/api/";
 export const DATE_FORMAT = "DD/MM/YYYY";
 export const DATE_TIME_FORMAT = "DD/MM/YYYY HH:mm:ss";
