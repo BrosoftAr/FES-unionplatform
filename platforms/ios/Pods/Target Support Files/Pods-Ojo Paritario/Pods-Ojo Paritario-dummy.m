72bc2af9917f2182512afb24ba129f932e9e20a6
Juan Ignacio Porta <juporta@frba.utn.edu.ar>
2023-09-22 12:53:23 -0300

    Modified platforms

--- platforms/ios/Pods/Target Support Files/Pods-Ojo Paritario/Pods-Ojo Paritario-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Ojo_Paritario : NSObject
-@end
-@implementation PodsDummy_Pods_Ojo_Paritario
-@end
--- platforms/ios/www/firebase-messaging-sw.js
+++ platforms/ios/www/firebase-messaging-sw.js
@@ -2,9 +2,8 @@
 // See `Access registration token section` @ https://firebase.google.com/docs/cloud-messaging/js/client#retrieve-the-current-registration-token
 
 // Scripts for firebase and firebase messaging
-importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
-
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
 
 // Initialize the Firebase app in the service worker by passing the generated config
 const firebaseConfig = {
@@ -14,9 +13,8 @@ const firebaseConfig = {
   storageBucket: "ojo-paritario.appspot.com",
   messagingSenderId: "793997687357",
   appId: "1:793997687357:web:5427d843790f4325b44854",
-  measurementId: "G-8GKLX44WXL"
+  measurementId: "G-8GKLX44WXL",
 };
-
 
 firebase.initializeApp(firebaseConfig);
 
@@ -24,14 +22,13 @@ firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 // Handle incoming messages while the app is not in focus (i.e in the background, hidden behind other tabs, or completely closed).
-messaging.onBackgroundMessage(function(payload) {
-  console.log('Received background message ', payload);
+messaging.onBackgroundMessage(function (payload) {
+  console.log("Received background message ", payload);
 
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
   };
 
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
